d29814c52d072c8652fe3239b5d66a52af10e92f
autocme <autocme20@gmail.com>
2025-06-15 12:37:32 +0000

    Moved server action logic from XML to Python method
    ✓ Created action_batch_payment_generate_link() in AccountMove model
    ✓ Simplified server action to call Python method

--- j_batch_payment_link/models/account_move.py
+++ j_batch_payment_link/models/account_move.py
@@ -66,3 +66,62 @@ class AccountMove(models.Model):
                 'active_model': 'account.move',
             }
         }
+    
+    def action_batch_payment_generate_link(self):
+        """Server action method for generating batch payment link."""
+        from odoo.exceptions import UserError
+        
+        # Validate selected invoices
+        if not self:
+            raise UserError(_('Please select at least one invoice.'))
+
+        # Filter only customer invoices
+        customer_invoices = self.filtered(lambda inv: inv.move_type == 'out_invoice')
+        if not customer_invoices:
+            raise UserError(_('Please select only customer invoices.'))
+
+        # Check if all invoices belong to the same partner
+        partners = customer_invoices.mapped('partner_id')
+        if len(partners) > 1:
+            raise UserError(_(
+                'All selected invoices must belong to the same customer. '
+                'Found invoices for multiple customers: %s'
+            ) % ', '.join(partners.mapped('name')))
+
+        # Check if all invoices are posted
+        non_posted = customer_invoices.filtered(lambda inv: inv.state != 'posted')
+        if non_posted:
+            raise UserError(_(
+                'All invoices must be posted before creating batch payment. '
+                'Non-posted invoices: %s'
+            ) % ', '.join(non_posted.mapped('name')))
+
+        # Check if all invoices have outstanding amounts
+        paid_invoices = customer_invoices.filtered(lambda inv: inv.amount_residual <= 0)
+        if paid_invoices:
+            raise UserError(_(
+                'All invoices must have outstanding amounts. '
+                'Already paid invoices: %s'
+            ) % ', '.join(paid_invoices.mapped('name')))
+
+        # Check if all invoices have the same currency
+        currencies = customer_invoices.mapped('currency_id')
+        if len(currencies) > 1:
+            raise UserError(_(
+                'All invoices must have the same currency. '
+                'Found multiple currencies: %s'
+            ) % ', '.join(currencies.mapped('name')))
+
+        # Open wizard to create batch payment
+        return {
+            'name': _('Create Batch Payment'),
+            'type': 'ir.actions.act_window',
+            'res_model': 'batch.payment.wizard',
+            'view_mode': 'form',
+            'target': 'new',
+            'context': {
+                'default_invoice_ids': [(6, 0, customer_invoices.ids)],
+                'active_ids': customer_invoices.ids,
+                'active_model': 'account.move',
+            }
+        }
