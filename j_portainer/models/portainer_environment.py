43a96e2dcbec028fbcb5686244b0ee439ee61156
autocme <autocme20@gmail.com>
2025-06-18 10:26:46 +0000

    fix action_view_images/action_view_networks using the self.id instead of the self.environment_id

--- j_portainer/models/portainer_environment.py
+++ j_portainer/models/portainer_environment.py
@@ -535,11 +535,11 @@ docker service create \\
             'view_mode': 'tree,form',
             'domain': [
                 ('server_id', '=', self.server_id.id),
-                ('environment_id', '=', self.environment_id)
+                ('environment_id', '=', self.id)
             ],
             'context': {
                 'default_server_id': self.server_id.id,
-                'default_environment_id': self.environment_id,
+                'default_environment_id': self.id,
                 'default_environment_name': self.name,
             }
         }
@@ -574,11 +574,11 @@ docker service create \\
             'view_mode': 'tree,form',
             'domain': [
                 ('server_id', '=', self.server_id.id),
-                ('environment_id', '=', self.environment_id)
+                ('environment_id', '=', self.id)
             ],
             'context': {
                 'default_server_id': self.server_id.id,
-                'default_environment_id': self.environment_id,
+                'default_environment_id': self.id,
                 'default_environment_name': self.name,
             }
         }
